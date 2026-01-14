af30c7cea813b7740062471bdde79d1266d18e3a
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2026-01-14 08:27:20 +0100

    feat: integration navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './Components/nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, NavBar],
+  templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('zero20site');
+  	protected readonly title = signal('zero20site');
+
+
 }
