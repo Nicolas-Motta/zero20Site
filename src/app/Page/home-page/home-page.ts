c183dba25711dd8b1391ac23ffb17cfd92c72342
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2026-01-16 23:44:28 +0100

    refractor: move fotter in home-page

--- src/app/Page/home-page/home-page.ts
+++ src/app/Page/home-page/home-page.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChildren } from '@angular/core';
 import { Box } from './Components/box/box'
 import { NavBar } from '../../Components/nav-bar/nav-bar';
+import { Footer } from '../../Components/footer/footer';
 
 @Component({
   selector: 'home-page',
-  imports: [NavBar, Box],
+  imports: [NavBar, Box, Footer],
   templateUrl: './home-page.html'
 })
 export class HomePage {
-  
 }
